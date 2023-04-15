6a1bb00af263aa244b7a567cccc06511193e16e9
SpySnakeEye <art.arh1@gmail.com>
2023-04-15 15:20:18 +0300

    hv05-GC_15.04.2023_15:19

---
+++ hw05-gc/src/main/java/ru/archipov/GcTests_2.java
@@ -0,0 +1,124 @@
+package ru.archipov;
+
+import java.util.ArrayList;
+
+public class GcTests_2 {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<String> num = new ArrayList<>();
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rh7garijg aijergija");
+            num.add("ljnrg nartegn oerg7 oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbr7khbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot7'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg7 ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui7,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg na77rtegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyilo7i,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa nearg o7aejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaer7h gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyiloi,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q7t3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrb7jaernbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh 7oeautruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyiloi7,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43fvtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnreoa 7nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oeaurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgari7jg aijergija");
+            num.add("ljnrg naregn oetrg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("rthrthrththrtyilo7i,ui,fiu,f,dghjdghjhbek5b");
+            num.add("ytjdyrt7gpeot'43fvtn9qt3'4 uq3'utqvnutnuq34");
+            num.add("lerhg;aeruht;noawryg owor;ug h;eoaurhg; ar");
+            num.add("ldjnrbjaer7nbnretoa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oe7autruhg ahe;rhgarijg aijergija");
+            num.add("ljnrg nartegn oerg oaergh o;aehrg aerhg; aoeurhg ");
+            num.add("Fkhkfbrkhbgekjb5he7.khjtbe5kjhbe5kjhbek5b");
+            num.add("rthrthrththrtyilo7i,ui,fiut,f,dghjdghjhbek5b");
+            num.add("ytjdyrtgpeot'43f7vtn9q3'4 uqt3'utqvnutnuq34");
+            num.add("ldjnrbjaernbnre7oa nearg oaejr ;ogija;eorig j");
+            num.add("huRGUhlerugh oe7aurhg otuaerh gu;hsrugh ;rg");
+            num.add("wuhg aiuhrgu haertuhg ahe;rhgarijg aijergija");
+            num.add("ljnrg naregn oe7trg oaergh o;aehrg aerhg; aoeurhg ");
+            num.remove(i);
+            num.remove(i+5);
+
+           /* for (String nums : num) {
+                 System.out.println(nums);
+            }
+
+            Thread.sleep(5000); */
+        }
+    }
+}
